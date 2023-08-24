8a43df1bb82f454ec3564f6f64955fc46c84d38e
Rory Houlding <rohoulding@googlemail.com>
2023-08-24 07:45:52 -0400

    Completed reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = '';
+    for (char of string) {
+        reversedString = char + reversedString;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
