e856d1a964f6ecd5d2a53c5f5e36440783e43656
Rory Houlding <rohoulding@googlemail.com>
2023-08-24 08:28:23 -0400

    Completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
 
+    if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return 'ERROR'
+    }
+
+    let lowestNum = num1 > num2 ? num2 : num1;
+    let highestNum = num1 > num2 ? num1 : num2;
+    let sum = 0;
+
+    for (let i = lowestNum; i <= highestNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
